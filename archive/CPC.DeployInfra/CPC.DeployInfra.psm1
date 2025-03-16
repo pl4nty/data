2045c9ca4bbb055cb9c1c4f0fe2c4bb5683d4de2
Tom Plant <tom@tplant.com.au>
2025-03-16 03:05:50 +0000

    fix: unarchive CPC.DeployInfra
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- archive/CPC.DeployInfra/CPC.DeployInfra.psm1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem (Split-Path $script:MyInvocation.MyCommand.Path) -Filter '*.ps1' -Recurse | ForEach-Object {
-    .$_.FullName
-}
-
-Import-Module Az -ErrorAction SilentlyContinue
-Import-Module Az.ManagedServiceIdentity -ErrorAction SilentlyContinue
