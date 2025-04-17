5ae8b827cf456cf82b8168fa12bcd1ccea3d0577
Tom Plant <tom@tplant.com.au>
2025-04-17 11:06:00 +1000

    fix: microsoft_3rdpartysource.py edge all files with name

--- microsoft_3rdpartysource.py
+++ microsoft_3rdpartysource.py
@@ -223,7 +223,7 @@ def download_zip_contents(zip_handle, exclude_paths=None, output_dir='msedge'):
             return False
         
         # Check for Edge files
-        if output_dir == 'msedge' and (filename.endswith('features.h') or filename.endswith('switches.h') or filename.startswith('edge')):
+        if output_dir == 'msedge' and (filename.endswith('features.h') or filename.endswith('switches.h') or 'edge' in filename):
             return True
         
         # Then check if it matches our extraction patterns
