6f845a92aa4bb9f4ab6997b526460d39eed5effa
Tom Plant <tom@tplant.com.au>
2025-05-01 01:10:11 +1000

    fix: uupdump.py all ESDs

--- uupdump.py
+++ uupdump.py
@@ -33,28 +33,44 @@ if updateId == existing_update_id:
 print('Found new update:', update['updateTitle'])
 
 files = request(f'https://api.uupdump.net/get.php?id={updateId}&lang=en-us&edition=professional').json()
-metadata_url = files['response']['files']['MetadataESD_professional_en-us.esd']['url']
-with tempfile.NamedTemporaryFile(suffix='.esd') as f:
-    metadata_file = f.name
-    response = requests.get(metadata_url, stream=True)
-    with open(metadata_file, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
+# esd_files = [
+#     'MetadataESD_professional_en-us.esd',
+#     'Microsoft-Windows-Client-Desktop-Required-Package.ESD'
+# ]
+esd_files = [filename for filename in files if filename.lower().endswith('.esd')]
+temp_dir = tempfile.mkdtemp()
+downloaded_files = {}
+for filename in esd_files:
+    if filename in files:
+        file_url = files[filename]['url']
+        file_path = os.path.join(temp_dir, filename)
+        downloaded_files[filename] = file_path
+        
+        print(f"Downloading {filename}...")
+        response = requests.get(file_url, stream=True)
+        with open(file_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+    else:
+        print(f"Warning: File {filename} not available in the update")
 
-    try:
-        subprocess.run(['sudo', 'apt-get', 'update'], check=True)
-        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'wimtools'], check=True)
+try:
+    subprocess.run(['sudo', 'apt-get', 'update'], check=True)
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'wimtools'], check=True)
+    
+    metadata_file = downloaded_files.get('MetadataESD_professional_en-us.esd')
+    if metadata_file:
         subprocess.run(['wiminfo', metadata_file], check=True)
         subprocess.run([
             'wimextract', metadata_file, '3',
             '/Windows/SystemApps/Microsoft.Windows.CloudExperienceHost_cw5n1h2txyewy',
             '--dest-dir=' + os.path.join(root, 'MetadataESD', 'Client'),
-            '--no-acls', '--preserve-dir-structure'
+            '--no-acls', '--preserve-dir-structure', '--ref=' + os.path.join(temp_dir, '*.esd')
         ], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Output: {e.output}")
-        print(f"Stderr: {e.stderr}")
-        raise e
-
+except subprocess.CalledProcessError as e:
+    print(f"Output: {e.output}")
+    print(f"Stderr: {e.stderr}")
+    raise e
+    
 with open(update_id_file, 'w') as f:
     f.write(updateId)
