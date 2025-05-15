ad6bf5054bdcf4e8c6796d015861d642439293a2
Tom Plant <tom@tplant.com.au>
2025-05-15 14:37:58 +0000

    fix: microsoft_defender_signatures.ps1
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- microsoft_defender_signatures.ps1
+++ microsoft_defender_signatures.ps1
@@ -1,19 +1,17 @@
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?LinkID=121721&arch=x64" -OutFile "..\mpam-fe.exe"
 7z x "..\mpam-fe.exe" -o"..\mpam-fe"
 
-Import-Module "$env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell -VsInstallPath "$env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise"
-cl /std:c++17 /EHsc extract_sig.cpp /Fe:..\extract_sig.exe
-
 cd ..\mpam-fe
 Invoke-WebRequest -Uri "https://github.com/hfiref0x/WDExtract/raw/refs/heads/master/Bin/bin64/wdextract64.exe" -OutFile "..\wdextract64.exe"
 Invoke-WebRequest -Uri "https://github.com/hfiref0x/WDExtract/raw/refs/heads/master/Bin/bin64/zlibwapi.dll" -OutFile "..\zlibwapi.dll"
 ..\wdextract64.exe mpasbase.vdm
 ..\wdextract64.exe mpasdlta.vdm
 
-..\extract_sig.exe . mpasbase.vdm.extracted mpasdlta.vdm.extracted
+Import-Module "$env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Enter-VsDevShell -VsInstallPath "$env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise"
+cl /std:c++17 /EHsc ..\data\extract_sig.cpp /Fe:extract_sig.exe
+.\extract_sig.exe . mpasbase.vdm.extracted mpasdlta.vdm.extracted
 Write-Host "Exit code: $LASTEXITCODE"
-ls
 
 Invoke-WebRequest -Uri "https://github.com/hongson11698/defender-database-extract/raw/refs/heads/master/lua_sig_parser_decompile.py" -OutFile "..\lua_sig_parser_decompile.py"
 python ..\lua_sig_parser_decompile.py lua_standalone_sig_mpasbase.vdm.extracted.csv
