09989d0407bba8e6bbc445445f8a14c6b336fd48
Tom Plant <tom@tplant.com.au>
2025-05-16 08:47:45 +0000

    fix: microsoft_defender_signatures.ps1
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- microsoft_defender_signatures.ps1
+++ microsoft_defender_signatures.ps1
@@ -7,8 +7,7 @@ Invoke-WebRequest -Uri "https://github.com/hfiref0x/WDExtract/raw/refs/heads/mas
 ..\wdextract64.exe mpasbase.vdm
 ..\wdextract64.exe mpasdlta.vdm
 
-Import-Module "$env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell -VsInstallPath "$env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise"
+& "$env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common6\Tools\Launch-VsDevShell.ps1" -Arch amd64
 cl /std:c++17 /EHsc ..\data\extract_sig.cpp
 .\extract_sig.exe . mpasbase.vdm.extracted mpasdlta.vdm.extracted
 Write-Host "Exit code: $LASTEXITCODE"
@@ -17,5 +16,5 @@ Invoke-WebRequest -Uri "https://github.com/hongson11698/defender-database-extrac
 Invoke-WebRequest -Uri "https://github.com/hongson11698/defender-database-extract/raw/refs/heads/master/lua_sig_parser_decompile.py" -OutFile "..\lua_sig_parser_decompile.py"
 python ..\lua_sig_parser_decompile.py lua_standalone_sig_mpasbase.vdm.extracted.csv
 Get-ChildItem .\lua -Recurse -Filter *_luac | Remove-Item
-Remove-Item ..\data\microsoft-defender-signatures -ErrorAction Ignore
+# Remove-Item ..\data\microsoft-defender-signatures -ErrorAction Ignore
 Move-Item .\lua ..\data\microsoft-defender-signatures
